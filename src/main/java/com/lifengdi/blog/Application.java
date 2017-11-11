7c847241053bba4269b165f395e1bafd9ca67538
lifengdi <34135685@qq.com>
2017-11-11 19:09:28 +0800

    首次上传

---
+++ src/main/java/com/lifengdi/blog/Application.java
@@ -0,0 +1,18 @@
+package com.lifengdi.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 项目启动类
+ * @author LiFengdi
+ * @date 2017年11月11日 下午6:47:04
+ */
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
+
+
