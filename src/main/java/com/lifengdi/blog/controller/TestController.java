13b9d0ad09adc96ce1659db4c9e47d6bb6afa7a3
lifengdi <34135685@qq.com>
2017-11-14 21:14:21 +0800

    页面测试

--- src/main/java/com/lifengdi/blog/controller/TestController.java
+++ src/main/java/com/lifengdi/blog/controller/TestController.java
@@ -2,10 +2,10 @@ package com.lifengdi.blog.controller;
 
 import javax.annotation.Resource;
 
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.lifengdi.blog.bean.City;
 import com.lifengdi.blog.mybatis.mapper.CityMapper;
@@ -15,15 +15,28 @@ import com.lifengdi.blog.mybatis.mapper.CityMapper;
  * @author LiFengdi
  * @date 2017年11月11日 下午7:47:05
  */
-@RestController
+@Controller
 public class TestController {
 	
 	@Resource  
     private CityMapper cityMapper;
 	
-	@RequestMapping("/hello/{name}")
-	String hello(@PathVariable String name, @RequestParam String param) {
-		return "Hello " + name + " param:" + param;
+	@RequestMapping("/asd")
+	String hello() {
+		System.out.println("22222");
+		return "/login";
+	}
+	
+	@RequestMapping("/index")
+	String index() {
+		System.out.println("11111");
+		return "index";
+	}
+	
+	@RequestMapping("/")
+	ModelAndView test() {
+		
+		return new ModelAndView("/index");
 	}
 	
 	@RequestMapping("/findCity")  
