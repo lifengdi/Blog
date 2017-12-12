ce9c37d52f5b8ffcb16b18857b9463bc5993de86
lifengdi <34135685@qq.com>
2017-12-12 18:40:06 +0800

    页面

---
+++ src/main/java/com/lifengdi/blog/controller/admin/TagController.java
@@ -0,0 +1,48 @@
+package com.lifengdi.blog.controller.admin;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.lifengdi.blog.bean.Tag;
+import com.lifengdi.blog.services.TagService;
+
+@Controller
+@RequestMapping("/admin/tag")
+public class TagController {
+	
+	@Autowired
+	private TagService tagService;
+	
+	private static final int TAG_TYPE = 0;
+	
+	@RequestMapping(value="/list", method=RequestMethod.GET)
+	@ResponseBody
+	Object list(HttpServletRequest request, HttpServletResponse response, @RequestParam int id) {
+		System.out.println(id);
+		JSONObject jsonObject = new JSONObject();
+		List<Tag> tags = tagService.selectByType(TAG_TYPE);
+		JSONArray jsonArray = new JSONArray();
+		if (!CollectionUtils.isEmpty(tags)) {
+			for (Tag tag : tags) {
+				jsonArray.add(JSON.toJSON(tag));
+			}
+		}
+		jsonObject.put("tags", jsonArray);
+		return jsonObject;
+	}
+
+}
+
---
+++ src/main/java/com/lifengdi/blog/services/TagService.java
@@ -0,0 +1,36 @@
+package com.lifengdi.blog.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lifengdi.blog.bean.Tag;
+import com.lifengdi.blog.bean.TagExample;
+import com.lifengdi.blog.mybatis.mapper.TagMapper;
+
+@Service
+public class TagService {
+
+	@Autowired
+	private TagMapper tagMapper;
+
+	/**
+	 * 根据类型查询标签
+	 * @author LiFengdi
+	 * @date 2017年12月12日 下午4:16:16
+	 * @param type
+	 * @return
+	 */
+	public List<Tag> selectByType(int type) {
+		try {
+			TagExample tagExample = new TagExample();
+			tagExample.createCriteria().andTypeEqualTo(type);
+			return tagMapper.selectByExample(tagExample);
+		} catch (Exception e) {
+			
+		}
+		
+		return null;
+	}
+}
